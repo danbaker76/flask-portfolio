f6c16a2b6f2e2abddadd50e167f3dbca976dce12
Dan Baker <danbaker@Dans-MacBook-Pro.local>
2026-05-30 12:14:50 -0600

    more updates

--- app.py
+++ app.py
@@ -19,8 +19,15 @@ def data_demo():
     # return render_template('data_demo.html', sales_data=sales_data)
     return render_template('data_demo.html')
 
+@app.route('/projects/<int:project_id>')
+def project_detail(project_id):
+    # You can pass dynamic data based on project_id
+    # For now, return a simple template
+    return render_template('project_detail.html', project_id=project_id)
+
 # This should be at the very bottom of your app.py file
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
