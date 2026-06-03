6aca3f6e9f83c4d0504a4e7d0249eae112bf5af5
Dan Baker <danbaker@Dans-MacBook-Pro.local>
2026-06-02 22:43:43 -0600

    updating stuff

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import sys, os
 
 app = Flask(__name__)
+CORS(app)   # enable CORS for the whole site (optional, but harmless)
 
 @app.route('/')
 def index():
-    # Rendering your home page template
     return render_template('index.html')
 
 @app.route('/projects')
 def projects():
-    # Rendering your projects listing page
     return render_template('projects.html')
 
 @app.route('/data-demo')
 def data_demo():
-    # For your interactive table, you'd pass data from a database or file
-    # sales_data = get_data() <-- You would need to implement this
-    # return render_template('data_demo.html', sales_data=sales_data)
     return render_template('data_demo.html')
 
 @app.route('/projects/<int:project_id>')
 def project_detail(project_id):
-    # You can pass dynamic data based on project_id
-    # For now, return a simple template
     return render_template('project_detail.html', project_id=project_id)
 
 @app.route('/ufc-predictor')
 def ufc_predictor():
     return render_template('ufc_predictor.html')
 
-# This should be at the very bottom of your app.py file
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
